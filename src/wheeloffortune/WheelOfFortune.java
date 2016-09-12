df718d5b9e16c5916659564e9deb7bb02c4cd964
yvs5276 <yvs5276@OZWDLND132-218.win.psu.edu>
2016-09-12 08:21:49 -0400

    Added Name and ID in the top file

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -3,8 +3,8 @@
 * Fall 2016
 * Instructor: Phil O'Connell
 * TODO: DON'T FORGET TO PUT YOUR NAME AND ID
-* Student: Stew Dent
-* ID: abc1234
+* Student: Yunnwei Swei
+* ID: yvs5276
 */
 package wheeloffortune;
 
