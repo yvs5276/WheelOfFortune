65bae0f92d1be928f9a82bfcb0db98f959a93de3
hoshi <hoshi@75.102.85.108>
2016-09-14 18:16:27 -0400

    Added codings for choices and scanner

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -6,6 +6,7 @@
 * ID: yvs5276
 */
 package wheeloffortune;
+import java.util.Scanner;
 
 public class WheelOfFortune {
 
@@ -13,7 +14,12 @@ public class WheelOfFortune {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    
+      String main;
+      String letter;
+      Scanner sc = new Scanner(System.in); 
+      
+      System.out.println("\nPress1. Spin the Wheel \nPress2. Buy a Vowel"
+              + "\nPress3. Solve the Puzzle\nPress4. Quit Game\nPress5. QA Tester");
   }
   
 }
