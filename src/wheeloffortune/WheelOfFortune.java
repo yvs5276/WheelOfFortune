c75eb11ce823dde34e94a7188ec386148da3b378
hoshi <hoshi@75.102.85.108>
2016-09-16 15:55:04 -0400

    Added 5 choices, if/else statements, and loops.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -20,6 +20,48 @@ public class WheelOfFortune {
       
       System.out.println("\nPress1. Spin the Wheel \nPress2. Buy a Vowel"
               + "\nPress3. Solve the Puzzle\nPress4. Quit Game\nPress5. QA Tester");
+      main = sc.next();
+      
+      while ((main != "0")){
+          if("1".equals(main)){
+            main = "Spinthe Wheel";
+            System.out.println(main);
+            System.out.println("Choose a letter");
+            letter = sc.next();
+            if(letter.matches("[A-Z]")){
+                  
+                  System.out.println("Your input: "+ letter);
+                  main();     
+                  
+              }
+              else{
+                  System.out.println("Invalid Input. Sorry, try again");
+                  main();
+                  
+              }
+          }
+          
+          if("2".equals(main)){
+             main = "Vowel Bought";
+             System.out.println(main);
+             main();
+          }
+           if("3".equals(main)){
+             main = "Solve Puzzle";
+             System.out.println(main);
+             main();
+          }         
+           if("4".equals(main)){
+             main = "Quit Game";
+             System.out.println(main);
+             System.exit(0);
+          }
+           if("5".equals(main)){
+             main = "QA Test Mode";
+             System.out.println(main);
+             main();
+          }
+      }
   }
   
 }
